6d186779ebb55b83d4c3442bf62645cf1e8dfa9d
İhsan ÇETİN <ihsancetin92@gmail.com>
2022-08-02 23:32:38 +0300

    Add files via upload

---
+++ appES5.js
@@ -0,0 +1,85 @@
+
+function Course(title, instructor, image) {
+  this.title = title;
+  this.instructor = instructor;
+  this.image = image;
+}
+
+//UI Const.
+function UI() {
+  UI.prototype.addCourseToList = function (course) {
+    const list = document.getElementById("course-list");
+    let html = `
+        <tr>
+        <td><img src="img/${course.image}"/></td>
+        <td>${course.title}</td>
+        <td>${course.instructor}</td>
+        <td><a href="#" class="btn btn-danger btn-sm delete">Delete</a></td>
+        
+        </tr>
+        `;
+
+    list.innerHTML += html;
+
+  };
+
+  UI.prototype.clearControls = function () {
+    const title = (document.getElementById("title").value = "");
+    const instructor = (document.getElementById("instructor").value = "");
+    const image = (document.getElementById("image").value = "");
+  };
+}
+UI.prototype.deleteCourse = function (element) {
+
+  if (element.classList.contains("delete")) {
+
+    element.parentElement.parentElement.remove();
+  }
+
+
+}
+UI.prototype.showAlert = function (message,className){
+
+  let alert = `
+  <div class="alert alert-${className}">${message}<div/>
+  
+  `;
+  const row = document.querySelector(".row");
+  row.insertAdjacentHTML('beforeBegin',alert);
+  setTimeout(() => { document.querySelector(".alert").remove();
+},3000)
+
+}
+
+document.getElementById("new-course").addEventListener("submit", function (e) {
+  const title = document.getElementById("title").value;
+  const instructor = document.getElementById("instructor").value;
+  const image = document.getElementById("image").value;
+
+  //CREATE NEW OBJ.
+  const course = new Course(title, instructor, image);
+
+  //create UI
+  const ui = new UI();
+  if (title === "" || image === "" || instructor === "") {
+
+    ui.showAlert("Please complete the form", "warning");
+  } else {
+    //Add course List
+    ui.addCourseToList(course);
+    
+
+    //clear controls
+
+    ui.clearControls();
+    ui.showAlert("The course has been added", "success");
+  }
+  e.preventDefault();
+
+});
+
+document.getElementById("course-list").addEventListener("click", function (e) {
+  const ui = new UI();
+  ui.deleteCourse(e.target);
+  ui.showAlert("The course has been deleted", "danger");
+})
